95c8413af80527a8ed0147f6191e59098232b4b2
Karter13 <KarMiH1975@mail.ru>
2020-12-01 23:07:09 +0300

    created Todo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import {View, Text, StyleSheet, TextInput, Button} from "react-native"
 
-export const AddTodo = () => {
+export const AddTodo = ({ onSubmit }) => {
+
+    const pressHandler = () => {
+        onSubmit('Test todo')
+    };
+
     return (
         <View style={styles.block}>
             <TextInput style={styles.input}/>
-            <Button title='Добавить'/>
+            <Button title='Добавить' onPress={pressHandler}/>
         </View>
     )
 };
@@ -14,13 +19,14 @@ const styles = StyleSheet.create({
     block: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: 15
     },
     input: {
         width: '70%',
         padding: 10,
         borderStyle: 'solid',
         borderBottomWidth: 2,
-        borderBottomColor: '#3949ab'
+        borderBottomColor: '#3949ab',
     }
 });
